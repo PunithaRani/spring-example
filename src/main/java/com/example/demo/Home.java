e79d1859bf2c555d6f4732f80c47f00980465c9b
PunithaRani <87994477+PunithaRani@users.noreply.github.com>
2022-01-18 22:31:27 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Home.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+	@RequestMapping("/home")
+	public String func(){
+		return "home hiiii";
+	}
+	
+}
